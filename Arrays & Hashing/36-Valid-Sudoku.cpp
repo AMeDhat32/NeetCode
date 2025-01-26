456c0c82273944243a08152404817a34fb41963b
abdelrahman medhat <126263611+AMeDhat32@users.noreply.github.com>
2025-01-27 00:42:48 +0200

    [36] [Time Beats: 100.00%] [Memory Beats: 67.15%] - LeetPush

--- Arrays & Hashing/36-Valid-Sudoku.cpp
+++ Arrays & Hashing/36-Valid-Sudoku.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
+        int row[9][9]{},col[9][9]{},SubBoard[9][9][9]{};
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char x = board[i][j];
+                if(x=='.')continue;
+                int number  = x - '1';
+                if(col[j][number]||row[i][number]||SubBoard[i/3 *3][j/3 *3][number])return 0;
+                col[j][number]++;
+                row[i][number]++;
+                SubBoard[i/3 *3][j/3 *3][number]++;
+            }
+        }
+
+
+        return 1;
     }
 };
