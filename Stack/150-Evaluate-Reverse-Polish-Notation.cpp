fe7502b520a42f2fcd870c0671d58e262a2ac6df
abdelrahman medhat <126263611+AMeDhat32@users.noreply.github.com>
2025-01-30 13:04:47 +0200

    [150] [Time Beats: 100.00%] [Memory Beats: 91.83%] - LeetPush

--- Stack/150-Evaluate-Reverse-Polish-Notation.cpp
+++ Stack/150-Evaluate-Reverse-Polish-Notation.cpp
@@ -2,25 +2,35 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int>sk;
-        unordered_map<string ,function<int(int,int)>> operation =
-        {
-          {\+\,[](int a,int b)->int { return a+b; } },
-          {\*\,[](int a,int b)->int { return a*b; } },
-          {\-\,[](int a,int b)->int { return a-b; } },
-          {\/\,[](int a,int b)->int { return a/b; } }
-        };
         for(auto& i :tokens){
-            if(operation.count(i)){
-                int x = sk.top(); sk.pop();
-                int y = sk.top(); sk.pop();
-                auto z = operation[i](y,x);
-                sk.push(z);
-            }else {
-                int x = stoi(i);
-                sk.push(x);
-            }
+          if(i==\+\){
+            int x = sk.top(); sk.pop();
+            int y = sk.top(); sk.pop();
+            int z = x+y;
+            sk.push(z);
+          }
+          else if(i==\*\){
+            int x = sk.top(); sk.pop();
+            int y = sk.top(); sk.pop();
+            int z = x*y;
+            sk.push(z);
+          }
+          else if(i==\-\){
+            int x = sk.top(); sk.pop();
+            int y = sk.top(); sk.pop();
+            int z = y-x;
+            sk.push(z);
+          }
+          else if(i==\/\){
+            int x = sk.top(); sk.pop();
+            int y = sk.top(); sk.pop();
+            int z = y/x;
+            sk.push(z);
+          }
+          else {
+            sk.push(stoi(i));
+          }
         }
-
         return sk.top();
     }
 };
